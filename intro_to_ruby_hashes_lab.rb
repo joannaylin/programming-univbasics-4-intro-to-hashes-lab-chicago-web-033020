b1c321cabea31951ddadaecbd87660fce0344e1d
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-09 16:38:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = {
-    key => value
+  hash.new {
+    :key => value
   }
 end
 
